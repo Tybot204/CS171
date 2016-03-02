bb5a3e2d542adb7415b66a4f9441ead04d4d7e1f
DANTE GARCIA RAMIREZ <dante@oomba.com>
2016-03-01 21:26:57 -0800

    J7 jar and J7 hotfix

--- SudokuProject/src/cspSolver/BTSolver.java
+++ SudokuProject/src/cspSolver/BTSolver.java
@@ -380,13 +380,14 @@ public class BTSolver implements Runnable{
 	public List<Integer> getValuesLCVOrder(Variable v)
 	{
 		List<Integer> values = v.getDomain().getValues();
+		final Variable tempv = v;
 		Comparator<Integer> valueComparator = new Comparator<Integer>(){
 
 			@Override
 			public int compare(Integer i1, Integer i2) {
 				Integer timesi1InDomainOfCells = 0;
 				Integer timesi2InDomainOfCells = 0;
-				for(Variable w: network.getNeighborsOfVariable(v)){
+				for(Variable w: network.getNeighborsOfVariable(tempv)){
 					if(!w.isAssigned()){
 						List<Integer> neighborDomain = w.getDomain().getValues();
 						if(neighborDomain.contains(i1)){
